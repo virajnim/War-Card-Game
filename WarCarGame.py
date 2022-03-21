b4beed92ec1e88473d3d89dcf5a33718427941d1
Viraj Nimmagadda <vnimmag@@asu.edu>
2022-03-20 22:25:06 -0700

    Fixed the infitie loop/only ending in a tie by shuffling the winnings of each round before putting it in the players deck.

--- WarCarGame.py
+++ WarCarGame.py
@@ -1,8 +1,10 @@
 import random
 from string import capwords
 
-# The creatDeck method creates a standard deck of 52 cards, shuffles that deck, and deals it to 2 players. It is dealt to each player one by one. It is not dealt by cutting the deck in half.
+# The creatDeck method creates a standard deck of 52 cards, shuffles that deck, and deals it to 2 players.
 # This method will return a distinct deck of 26 cards to each player.
+# input -> N/A
+# output -> 2 lists of cards
 def createDeck():
     suites = ["Hearts", "Clubs", "Diamonds", "Spades" ]
     deck = []
@@ -12,21 +14,18 @@ def createDeck():
         for i in range(1,14):
             deck.append([i , suite])
     random.shuffle(deck)
-    #print(deck)
 
-    for i in range(0,len(deck),2):
-        p1_cards.append(deck[i])
-        p2_cards.append(deck[i+1])
+    p1_cards = deck[0:26]
+    p2_cards = deck[26: ]
 
-    #print("\n Player 1 Deck:", p1_cards)
-    #print("\n Player 2 Deck" ,p2_cards)
-
-    #print('\n Return Value:', p1_cards.pop(0))
-    #print("\n Player 1 Deck:", p1_cards)
     return p1_cards, p2_cards
 
+
+ # The num_to_card method uses a dictionary to equate the card numbers to their respective cards and print out the cards for the game.
+ # input: 2 lists   
+ # output: a print statement of the players cards
 def num_to_card(p1_card, p2_card):
-    numdict = {
+    numdict = {                    
         1: "2",
         2: "3",
         3: "4",
@@ -45,6 +44,10 @@ def num_to_card(p1_card, p2_card):
     p2_str = numdict[p2_card[0]] + " of " + p2_card[1]
     print("{:40}{:40}".format(p1_str, p2_str), end='')
 
+
+# The intro method starts the gameplay. You can read the rules of the game or you can start playing.
+# input: N/A
+# output: print statements to guide the player through the game.
 def intro():
     print("Hello, welcome to the game of war! Press 0 to read the game rules or press 1 to start the game")
     val = input()
@@ -58,6 +61,10 @@ def intro():
         print("Invalid option")
         intro()
 
+
+# The winner method prints out the winner of the game
+# input: 2 lists (of the players decks)
+# output: print statements
 def winner(p1,p2):
     if len(p2) == 0:
             if len(p1) == 0:
@@ -70,49 +77,50 @@ def winner(p1,p2):
     return True
 
 
+# The play_war method holds all the game-play logic. It goes over the 3 conditions of the game by using a while loop with 3 ifs statement.
+# input: 2 lists (of the players decks)
+# output: N/A
 def play_war(p1, p2):
     
     intro()
     print("game has started here")
     print("{:40}{:40}".format("Player 1", "Player 2"), 'Result.\n', sep='')
-    count = 1
     p1_4cards = []
     p2_4cards = []
 
-    while len(p1) >=0 and len(p2) >=0:
-
-        if len(p1) <= 0 or len(p2) <= 0:
-            return winner(p1,p2)
+    while len(p1) != 0 and len(p2) != 0:   # while the length of both players decks does not equal 0
  
-        p1_card = p1.pop(0)
+        p1_card = p1.pop(0)                # get the first cards from the players decks
         p2_card = p2.pop(0)
-        #print(p1_card, p2_card)
-        #print(len(p1), " ", len(p2))
-        num_to_card(p1_card, p2_card)
-        if p2_card[0] == p1_card[0]:
-            p1_4cards.extend([p1_card] + p1[0:4])
-            del p1[0:4]
 
-            p2_4cards.extend([p2_card] + p2[0:4])
-            del p2[0:4]
+        num_to_card(p1_card, p2_card)
+
+        if p2_card[0] == p1_card[0]:       # if the cards are equal, put that card and the next 3 cards in a temp deck.
+            p1_4cards = p1_4cards + [p1_card] + p1[0:3]
+            p1 = p1[3:]
+
+            p2_4cards = p2_4cards + [p2_card] + p2[0:3]
+            p2 = p2[3:]
+            random.shuffle(p1_4cards)      # you have to shuffle the winnings in order to avoid an infinite loop
+            random.shuffle(p2_4cards)
             print("Tie.\n")
-        elif p1_card[0] > p2_card[0]:
-            #p1.extend([p1_card, p2_card, p1_4cards, p2_4cards])
+
+        elif p1_card[0] > p2_card[0]:      # if player 1's card is greater than player 2's card add both cards to the bottom of players 1's deck 
             p1 = p1 + [p1_card] + [p2_card] + p2_4cards + p1_4cards
             p1_4cards = []
             p2_4cards = []
             print("Player 1 takes the cards.\n")
             
         
-        elif p2_card[0] > p1_card[0]:
-            #p2.extend([p2_card, p1_card, p1_4cards, p2_4cards])
-            p2 = p2 + [p1_card] + [p2_card] + p2_4cards + p1_4cards
+        elif p2_card[0] > p1_card[0]:      # if player 2's card is greater than player 1's card add both cards to the bottom of players 2's deck
+            p2 = p2 + [p2_card] + [p1_card] + p2_4cards + p1_4cards
             p1_4cards = []
             p2_4cards = []
             print("Player 2 takes the cards.\n")
-            if len(p1) <= 0 or len(p2) <= 0:
-                return winner(p1,p2)
-        count += 1
+            
+
+    if len(p1) <= 0 or len(p2) <= 0:       # if the length of either players decks is 0 or less, return the winner of the game.
+        return winner(p1,p2)
     return True
 
 
@@ -120,4 +128,5 @@ def play_war(p1, p2):
 
 if __name__ == "__main__":
     p1, p2 = createDeck()
+
     play_war(p1, p2)
